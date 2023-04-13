39f29fff1bf4513e4b56bccb28b277dd765a471c
tkazevicius <t.kazevicius@gmail.com>
2023-04-13 16:52:19 +0300

    Added data processing functions of the calculate form.

---
+++ src/main/java/com/it/academy/mortgage/calculator/exceptions/CalculatorException.java
@@ -0,0 +1,7 @@
+package com.it.academy.mortgage.calculator.exceptions;
+
+public class CalculatorException extends RuntimeException {
+    public CalculatorException() {
+        super("Error");
+    }
+}
