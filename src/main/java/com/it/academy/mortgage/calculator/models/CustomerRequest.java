413ade7ed8aab2229bff441a8fa56d1cc57a0447
linas159 <linas.mintautiskis@gmail.com>
2023-04-21 14:40:50 +0300

    resolved conflict4??

--- src/main/java/com/it/academy/mortgage/calculator/models/CustomerRequest.java
+++ src/main/java/com/it/academy/mortgage/calculator/models/CustomerRequest.java
@@ -1,13 +1,17 @@
 package com.it.academy.mortgage.calculator.models;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Length;
 
 public record CustomerRequest(
+        @NotEmpty(message = "Name cannot be empty")
         String name,
+        @Length(min = 1, message = "Phone number too short")
+        @Length(max = 20, message = "Phone number too long")
         String phoneNumber,
+        @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+        @NotEmpty(message = "Email cannot be empty")
         String email,
         String action) {
 }
