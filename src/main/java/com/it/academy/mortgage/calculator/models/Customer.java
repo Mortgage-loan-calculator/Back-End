d08dd7a92af8c6775e298beffbdbe3a28da66b27
unknown <Linas@Jegaine.local>
2023-04-24 22:03:49 +0300

    Customer detailed and delete functionality added in admin panel

--- src/main/java/com/it/academy/mortgage/calculator/models/Customer.java
+++ src/main/java/com/it/academy/mortgage/calculator/models/Customer.java
@@ -1,10 +1,7 @@
 package com.it.academy.mortgage.calculator.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.Length;
 
 import java.net.InetAddress;
@@ -31,18 +28,37 @@ public class Customer {
     private String ipAddress;
     private final LocalDateTime time;
     private String action;
+    @NotNull
+    @Min(value = 0L, message = "familyMembers must be positive")
+    private Integer familyMembers;
+    @NotNull
+    private Boolean haveChildren;
+    @NotNull
+    @Min(value = 0L, message = "homePrice must be positive")
+    private Integer homePrice;
+    @NotNull
+    @Min(value = 0L, message = "loanTerm must be positive")
+    private Integer loanTerm;
+    @NotNull
+    @Min(value = 0L, message = "monthlyFamilyIncome must be positive")
+    private Integer monthlyFamilyIncome;
 
     public Customer() {
         this.time = LocalDateTime.now();
     }
 
-    public Customer(String name, String phoneNumber, String email, String action) throws UnknownHostException {
+    public Customer(String name, String phoneNumber, String email, String action, Integer familyMembers, Boolean haveChildren, Integer homePrice, Integer loanTerm, Integer monthlyFamilyIncome) throws UnknownHostException {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.ipAddress = InetAddress.getLocalHost().getHostAddress();
         this.time = LocalDateTime.now();
         this.action = action;
+        this.familyMembers = familyMembers;
+        this.haveChildren = haveChildren;
+        this.homePrice = homePrice;
+        this.loanTerm = loanTerm;
+        this.monthlyFamilyIncome = monthlyFamilyIncome;
     }
 
     public String getId() {
@@ -95,6 +111,46 @@ public class Customer {
 
     public void setAction(String action) {
         this.action = action;
+    }
+
+    public Integer getFamilyMembers() {
+        return familyMembers;
+    }
+
+    public void setFamilyMembers(Integer familyMembers) {
+        this.familyMembers = familyMembers;
+    }
+
+    public Boolean getHaveChildren() {
+        return haveChildren;
+    }
+
+    public void setHaveChildren(Boolean haveChildren) {
+        this.haveChildren = haveChildren;
+    }
+
+    public Integer getHomePrice() {
+        return homePrice;
+    }
+
+    public void setHomePrice(Integer homePrice) {
+        this.homePrice = homePrice;
+    }
+
+    public Integer getLoanTerm() {
+        return loanTerm;
+    }
+
+    public void setLoanTerm(Integer loanTerm) {
+        this.loanTerm = loanTerm;
+    }
+
+    public Integer getMonthlyFamilyIncome() {
+        return monthlyFamilyIncome;
+    }
+
+    public void setMonthlyFamilyIncome(Integer monthlyFamilyIncome) {
+        this.monthlyFamilyIncome = monthlyFamilyIncome;
     }
 
     @Override
