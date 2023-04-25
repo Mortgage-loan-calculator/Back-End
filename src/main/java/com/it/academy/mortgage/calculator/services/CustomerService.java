e201a58987a37b91d88c1f89e68ed49db0591f19
unknown <Linas@Jegaine.local>
2023-04-25 21:14:45 +0300

    email sender

--- src/main/java/com/it/academy/mortgage/calculator/services/CustomerService.java
+++ src/main/java/com/it/academy/mortgage/calculator/services/CustomerService.java
@@ -4,6 +4,7 @@ import com.it.academy.mortgage.calculator.exceptions.CustomerNotFoundException;
 import com.it.academy.mortgage.calculator.models.Customer;
 import com.it.academy.mortgage.calculator.models.CustomerRequest;
 import com.it.academy.mortgage.calculator.repositories.CustomerRepository;
+import jakarta.validation.constraints.Email;
 import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +18,11 @@ import java.util.stream.Collectors;
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final EmailSenderService emailSenderService;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, EmailSenderService emailSenderService) {
         this.customerRepository = customerRepository;
+        this.emailSenderService = emailSenderService;
     }
 
     public List<Customer> fetchAllCustomers() {return customerRepository.findAll();}
@@ -37,6 +40,7 @@ public class CustomerService {
                 newCustomer.monthlyFamilyIncome()
         );
         customerRepository.save(customer);
+        sendEmailToCustomer(customer);
     }
 
     public void deleteCustomerById(String id) {customerRepository.deleteById(id);}
@@ -47,5 +51,14 @@ public class CustomerService {
 
     public ResponseEntity<Customer> fetchCustomerById(String id) throws CustomerNotFoundException {
         return new ResponseEntity<>(customerRepository.findById(id).orElseThrow(() -> new CustomerNotFoundException(id)), HttpStatus.ACCEPTED);
+    }
+
+    private void sendEmailToCustomer(Customer customer){
+        emailSenderService.sendEmail(
+                customer.getEmail(),
+                "Application for mortgage at Team SwEB",
+                "We got your information and will get back to you shortly!!! \n "
+        );
+
     }
 }
