03a9f1959748ba9b78ec86d0e65e4c7ce5f2b905
Aurelija Milkaitė-Stasiukevičė <109099811+aurelija02@users.noreply.github.com>
2023-04-28 14:44:42 +0300

    Update CustomerRequest.java

--- src/main/java/com/it/academy/mortgage/calculator/models/CustomerRequest.java
+++ src/main/java/com/it/academy/mortgage/calculator/models/CustomerRequest.java
@@ -7,8 +7,7 @@ import org.hibernate.validator.constraints.Length;
 public record CustomerRequest(String id,
                               @NotEmpty(message = "Name cannot be empty")
                               String name,
-                              @Length(max = 20, message = "Phone number too long")
-                              @Digits(integer = 20, fraction = 0, message = "Phone number has to be a number")
+                              @Pattern(regexp = "^\\d*$", message = "Phone number should only contain positive numbers")
                               String phoneNumber,
                               @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
                               @NotEmpty(message = "Email cannot be empty")
